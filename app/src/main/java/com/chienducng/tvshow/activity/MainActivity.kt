be71c505443ddce1a0143c23bae75e9af0cd9a99
chienducng <chienducng@gmail.com>
2020-10-15 22:36:59 +0700

    Pagination In TV Show List | Endless Recycler View

--- app/src/main/java/com/chienducng/tvshow/activity/MainActivity.kt
+++ app/src/main/java/com/chienducng/tvshow/activity/MainActivity.kt
@@ -2,9 +2,11 @@ package com.chienducng.tvshow.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.chienducng.tvshow.R
 import com.chienducng.tvshow.adapters.TVShowsAdapter
 import com.chienducng.tvshow.databinding.ActivityMainBinding
@@ -12,12 +14,15 @@ import com.chienducng.tvshow.models.TvShow
 import com.chienducng.tvshow.viewmodes.MostPopularTVShowViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val TAG: String = "MainActivity"
     private lateinit var mostPopularTVShowViewModel : MostPopularTVShowViewModel
     private lateinit var activityMainBinding: ActivityMainBinding
     private var tvShows : ArrayList<TvShow> = ArrayList()
     private val tvShowsAdapter by lazy {
         TVShowsAdapter(tvShows)
     }
+    private var currentPage = 1;
+    private var totalAvailablePage = 1;
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,17 +35,39 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding.tvShowsRecyclerView.setHasFixedSize(true)
         mostPopularTVShowViewModel = ViewModelProvider(this).get(MostPopularTVShowViewModel::class.java)
         activityMainBinding.tvShowsRecyclerView.adapter = tvShowsAdapter
+        activityMainBinding.tvShowsRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (!activityMainBinding.tvShowsRecyclerView.canScrollVertically(1)) {
+                    if (currentPage <= totalAvailablePage) {
+                        currentPage += 1;
+                        getMostPopularTVShows()
+                    }
+                }
+            }
+        })
         getMostPopularTVShows()
     }
 
     private fun getMostPopularTVShows() {
-        activityMainBinding.isLoading = true
-        mostPopularTVShowViewModel.getMostPopularTVShows(0).observe(this, Observer {
-            activityMainBinding.isLoading = false
+        toggleLoading()
+        mostPopularTVShowViewModel.getMostPopularTVShows(currentPage).observe(this, Observer{
+            toggleLoading()
             if (it != null) {
+                totalAvailablePage = it.pages
+                Log.i(TAG, "total: $totalAvailablePage")
+                val oldCount = tvShows.size
                 tvShows.addAll(it.tvShows)
-                tvShowsAdapter.notifyDataSetChanged()
+                tvShowsAdapter.notifyItemRangeInserted(oldCount, tvShows.size)
             }
         })
+    }
+
+    private fun toggleLoading() {
+        if (currentPage == 1) {
+            activityMainBinding.isLoading = !activityMainBinding.isLoading
+        } else {
+            activityMainBinding.isLoadingMore = !activityMainBinding.isLoadingMore
+        }
     }
 }
